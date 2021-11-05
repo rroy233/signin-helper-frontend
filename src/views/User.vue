fe7c60772e73a9a74a50ab349df7be463602ad22
Roy <oisroy233@gmail.com>
2021-11-05 21:56:13 +0800

    init

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <user-index/>
+</template>
+
+<script>
+import UserIndex from "../components/User/Index.vue"
+export default {
+    name:"User",
+    components:{
+        UserIndex,
+    }
+}
+</script>
+
+<style>
+
+</style>
